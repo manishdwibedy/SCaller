56ebd5a6b9beeb968952e8db8cf0540c512f3afe
Manish Dwibedy <manish.dwibedy@gmail.com>
2015-12-13 22:35:26 -0800

    Icons now working..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,17 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home</title>
+    <title>{{Config::get('constants.SITE_NAME')}} | Home</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
     <!-- Bootstrap 3.3.5 -->
     {!! HTML::style('bootstrap/css/bootstrap.min.css') !!}
 
     <!-- Font Awesome -->
-    {!! HTML::style('css/font-awesome.min.css') !!}
-
+    {!! HTML::style('https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css') !!}
     <!-- Ionicons -->
-    {!! HTML::style('css/ionicons.min.css') !!}
+    {!! HTML::style('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') !!}
 
     <!-- Theme style -->
     {!! HTML::style('css/AdminLTE.min.css') !!}
