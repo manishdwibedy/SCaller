57b6ff27bd81ceba233a06d08db6fbb6e4d3c500
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-01-07 14:18:06 -0800

    Using the favicon icon noew...

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -4,6 +4,9 @@
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
+<link rel="shortcut icon" href="{{ asset('images/logo.png') }}">
+
+
 <!-- Bootstrap 3.3.5 -->
 {!! HTML::style('bootstrap/css/bootstrap.min.css') !!}
 
