9079ac1342a7ae70cbd7dde858bda7e7a7e56f14
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-01-12 13:56:18 -0800

    Improved the UI of the inbox and reading individual messages.

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -40,69 +40,129 @@ hr { margin-top: 5px;margin-bottom: 10px; }
         </section>
 
         <!-- Main content -->
+
         <section class="content">
-          <div class="container-fluid">
-
-
-              <div class="tab-content">
-                <div class="tab-pane fade in active" id="home">
-                    <div class="list-group">
-                        @foreach($threads as $thread)
-                            @if( $thread->messages[0]->user->id != Auth::user()->id )
-                            <a href="getMessages/{{ $thread->id }}" class="list-group-item">
-                                <span class="checkbox">
-                                    <label>
-                                        <input type="checkbox">
-                                    </label>
-                                </span>
-                                <span class="glyphicon glyphicon-star-empty"></span>
-                                <span class="name" style="min-width: 120px;
-                                    display: inline-block;">
-                                    {!! $thread->creator()->name !!}
-                                </span>
-                                <span class="">
-                                    {!! $thread->subject !!}
-                                </span>
-                                <span class="text-muted" style="font-size: 11px;">
-                                    {!! $thread->latestMessage->body !!}
-                                </span>
-                                <span class="badge">
-                                    @if (Carbon\Carbon::parse($thread->created_at)->format('d/m/Y')
-                                                == Carbon\Carbon::parse(Carbon\Carbon::now())->format('d/m/Y'))
-                                        {!! Carbon\Carbon::parse($thread->created_at)->format('h:i A') !!}
-                                    @else
-                                        {!! Carbon\Carbon::parse($thread->created_at)->format('d/m/Y') !!}
-                                    @endif
-
-                                </span>
-                                <span class="pull-right">
-                                    <!--
-                                        Not using attachments
-                                        <span class="glyphicon glyphicon-paperclip">
-                                    -->
-                                </span>
-                                </span>
-                            </a>
+          <div class="row">
+            <div class="col-md-3">
+              <a href="compose.html" class="btn btn-primary btn-block margin-bottom">Compose</a>
+              <div class="box box-solid">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Folders</h3>
+                  <div class="box-tools">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                  </div>
+                </div>
+                <div class="box-body no-padding">
+                  <ul class="nav nav-pills nav-stacked">
+                    <li class="active"><a href="#"><i class="fa fa-inbox"></i> Inbox <span class="label label-primary pull-right">12</span></a></li>
+                    <li><a href="#"><i class="fa fa-envelope-o"></i> Sent</a></li>
+                    <li><a href="#"><i class="fa fa-file-text-o"></i> Drafts</a></li>
+                    <li><a href="#"><i class="fa fa-filter"></i> Junk <span class="label label-warning pull-right">65</span></a></li>
+                    <li><a href="#"><i class="fa fa-trash-o"></i> Trash</a></li>
+                  </ul>
+                </div><!-- /.box-body -->
+              </div><!-- /. box -->
+              <div class="box box-solid">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Labels</h3>
+                  <div class="box-tools">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                  </div>
+                </div>
+                <div class="box-body no-padding">
+                  <ul class="nav nav-pills nav-stacked">
+                    <li><a href="#"><i class="fa fa-circle-o text-red"></i> Important</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> Promotions</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o text-light-blue"></i> Social</a></li>
+                  </ul>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+            <div class="col-md-9">
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Inbox</h3>
+                  <div class="box-tools pull-right">
+                    <div class="has-feedback">
+                      <input type="text" class="form-control input-sm" placeholder="Search Mail">
+                      <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                    </div>
+                  </div><!-- /.box-tools -->
+                </div><!-- /.box-header -->
+                <div class="box-body no-padding">
+                  <div class="mailbox-controls">
+                    <!-- Check all button -->
+                    <button class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i></button>
+                    <div class="btn-group">
+                      <button class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
+                      <button class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
+                      <button class="btn btn-default btn-sm"><i class="fa fa-share"></i></button>
+                    </div><!-- /.btn-group -->
+                    <button class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button>
+                    <div class="pull-right">
+                      1-50/200
+                      <div class="btn-group">
+                        <button class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
+                        <button class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
+                      </div><!-- /.btn-group -->
+                    </div><!-- /.pull-right -->
+                  </div>
+                  <div class="table-responsive mailbox-messages">
+                    <table class="table table-hover table-striped">
+                      <tbody>
+                          @foreach($threads as $thread)
+                              @if( $thread->messages[0]->user->id != Auth::user()->id )
+                                <tr>
+                                  <td><input type="checkbox"></td>
+                                  <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
+                                  <td class="mailbox-name">
+                                      <a href="getMessages/{{ $thread->id }}">{!! $thread->creator()->name !!}</a>
+                                  </td>
+                                  <td class="mailbox-subject">
+                                      <b>{!! $thread->subject !!}</b> - {!! $thread->latestMessage->body !!}
+                                  </td>
+                                  <td class="mailbox-attachment"></td>
+                                  <td class="mailbox-date">
+                                      @if (Carbon\Carbon::parse($thread->created_at)->format('d/m/Y')
+                                                  == Carbon\Carbon::parse(Carbon\Carbon::now())->format('d/m/Y'))
+                                          {!! Carbon\Carbon::parse($thread->created_at)->format('h:i A') !!}
+                                      @else
+                                          {!! Carbon\Carbon::parse($thread->created_at)->format('d/m/Y') !!}
+                                      @endif
+                                  </td>
+                                </tr>
                             @endif
-
                         @endforeach
-
-                    </div>
+                      </tbody>
+                    </table><!-- /.table -->
+                  </div><!-- /.mail-box-messages -->
+                </div><!-- /.box-body -->
+                <div class="box-footer no-padding">
+                  <div class="mailbox-controls">
+                    <!-- Check all button -->
+                    <button class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i></button>
+                    <div class="btn-group">
+                      <button class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
+                      <button class="btn btn-default btn-sm"><i class="fa fa-reply"></i></button>
+                      <button class="btn btn-default btn-sm"><i class="fa fa-share"></i></button>
+                    </div><!-- /.btn-group -->
+                    <button class="btn btn-default btn-sm"><i class="fa fa-refresh"></i></button>
+                    <div class="pull-right">
+                      1-50/200
+                      <div class="btn-group">
+                        <button class="btn btn-default btn-sm"><i class="fa fa-chevron-left"></i></button>
+                        <button class="btn btn-default btn-sm"><i class="fa fa-chevron-right"></i></button>
+                      </div><!-- /.btn-group -->
+                    </div><!-- /.pull-right -->
+                  </div>
                 </div>
-                <div class="tab-pane fade in" id="profile">
-                    <div class="list-group">
-                        <div class="list-group-item">
-                            <span class="text-center">This tab is empty.</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade in" id="messages">
-                    ...</div>
-                <div class="tab-pane fade in" id="settings">
-                    This tab is empty.</div>
-            </div>
-          </div>
+              </div><!-- /. box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
         </section><!-- /.content -->
+
+
+        
       </div><!-- /.content-wrapper -->
 
       <!-- Footer -->
--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -17,62 +17,137 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
           <h1>
-            Inbox
+            View Message
           </h1>
           <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Inbox</li>
+            <li class="active">View Message</li>
           </ol>
         </section>
 
         <!-- Main content -->
+
         <section class="content">
-          <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-3">
+              <a href="compose.html" class="btn btn-primary btn-block margin-bottom">Compose</a>
+              <div class="box box-solid">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Folders</h3>
+                  <div class="box-tools">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                  </div>
+                </div>
+                <div class="box-body no-padding">
+                  <ul class="nav nav-pills nav-stacked">
+                    <li><a href="/getMessages"><i class="fa fa-inbox"></i> Inbox <span class="label label-primary pull-right">12</span></a></li>
+                    <li><a href="#"><i class="fa fa-envelope-o"></i> Sent</a></li>
+                    <li><a href="#"><i class="fa fa-file-text-o"></i> Drafts</a></li>
+                    <li><a href="#"><i class="fa fa-filter"></i> Junk <span class="label label-warning pull-right">65</span></a></li>
+                    <li><a href="#"><i class="fa fa-trash-o"></i> Trash</a></li>
+                  </ul>
+                </div><!-- /.box-body -->
+              </div><!-- /. box -->
+              <div class="box box-solid">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Labels</h3>
+                  <div class="box-tools">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                  </div>
+                </div>
+                <div class="box-body no-padding">
+                  <ul class="nav nav-pills nav-stacked">
+                    <li><a href="#"><i class="fa fa-circle-o text-red"></i> Important</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> Promotions</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o text-light-blue"></i> Social</a></li>
+                  </ul>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+            <div class="col-md-9">
+              <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Read Mail</h3>
+                  <div class="box-tools pull-right">
+                    <a href="#" class="btn btn-box-tool" data-toggle="tooltip" title="Previous"><i class="fa fa-chevron-left"></i></a>
+                    <a href="#" class="btn btn-box-tool" data-toggle="tooltip" title="Next"><i class="fa fa-chevron-right"></i></a>
+                  </div>
+                </div><!-- /.box-header -->
 
-              <h1>{!! $thread->subject !!}</h1>
-
-              @foreach($thread->messages as $message)
-
-                   <div class="media">
-                      <!-- <a class="pull-left" href="#">
-                          <img src="//www.gravatar.com/avatar/{!! md5($message->user->email) !!}?s=64" alt="{!! $message->user->name !!}" class="img-circle">
-                      </a> -->
-                      <div class="media-body">
-                          <h5 class="media-heading">{!! $message->user->name !!}</h5>
-                          <p>{!! $message->body !!}</p>
-                          <div class="text-muted"><small>Posted {!! $message->created_at->diffForHumans() !!}</small></div>
+                @foreach($thread->messages as $message)
+                <div class="box-body no-padding">
+                  <div class="mailbox-read-info">
+                    <h3>{!! $thread->subject !!}</h3>
+                    <h5>From:  {!! $message->user->name !!} <span class="mailbox-read-time pull-right">15 Feb. 2015 11:03 PM</span></h5>
+                  </div><!-- /.mailbox-read-info -->
+                  <div class="mailbox-controls with-border text-center">
+                    <div class="btn-group">
+                      <button class="btn btn-default btn-sm" data-toggle="tooltip" title="Delete"><i class="fa fa-trash-o"></i></button>
+                      <button class="btn btn-default btn-sm" data-toggle="tooltip" title="Reply"><i class="fa fa-reply"></i></button>
+                      <button class="btn btn-default btn-sm" data-toggle="tooltip" title="Forward"><i class="fa fa-share"></i></button>
+                    </div><!-- /.btn-group -->
+                    <button class="btn btn-default btn-sm" data-toggle="tooltip" title="Print"><i class="fa fa-print"></i></button>
+                  </div><!-- /.mailbox-controls -->
+                  <div class="mailbox-read-message">
+                    {!! $message->body !!}
+                  </div><!-- /.mailbox-read-message -->
+                </div><!-- /.box-body -->
+                @endforeach
+                <!-- <div class="box-footer">
+                  <ul class="mailbox-attachments clearfix">
+                    <li>
+                      <span class="mailbox-attachment-icon"><i class="fa fa-file-pdf-o"></i></span>
+                      <div class="mailbox-attachment-info">
+                        <a href="#" class="mailbox-attachment-name"><i class="fa fa-paperclip"></i> Sep2014-report.pdf</a>
+                        <span class="mailbox-attachment-size">
+                          1,245 KB
+                          <a href="#" class="btn btn-default btn-xs pull-right"><i class="fa fa-cloud-download"></i></a>
+                        </span>
                       </div>
+                    </li>
+                    <li>
+                      <span class="mailbox-attachment-icon"><i class="fa fa-file-word-o"></i></span>
+                      <div class="mailbox-attachment-info">
+                        <a href="#" class="mailbox-attachment-name"><i class="fa fa-paperclip"></i> App Description.docx</a>
+                        <span class="mailbox-attachment-size">
+                          1,245 KB
+                          <a href="#" class="btn btn-default btn-xs pull-right"><i class="fa fa-cloud-download"></i></a>
+                        </span>
+                      </div>
+                    </li>
+                    <li>
+                      <span class="mailbox-attachment-icon has-img"><img src="../../dist/img/photo1.png" alt="Attachment"></span>
+                      <div class="mailbox-attachment-info">
+                        <a href="#" class="mailbox-attachment-name"><i class="fa fa-camera"></i> photo1.png</a>
+                        <span class="mailbox-attachment-size">
+                          2.67 MB
+                          <a href="#" class="btn btn-default btn-xs pull-right"><i class="fa fa-cloud-download"></i></a>
+                        </span>
+                      </div>
+                    </li>
+                    <li>
+                      <span class="mailbox-attachment-icon has-img"><img src="../../dist/img/photo2.png" alt="Attachment"></span>
+                      <div class="mailbox-attachment-info">
+                        <a href="#" class="mailbox-attachment-name"><i class="fa fa-camera"></i> photo2.png</a>
+                        <span class="mailbox-attachment-size">
+                          1.9 MB
+                          <a href="#" class="btn btn-default btn-xs pull-right"><i class="fa fa-cloud-download"></i></a>
+                        </span>
+                      </div>
+                    </li>
+                  </ul>
+              </div> --> <!-- /.box-footer -->
+                <div class="box-footer">
+                  <div class="pull-right">
+                    <button class="btn btn-default"><i class="fa fa-reply"></i> Reply</button>
+                    <button class="btn btn-default"><i class="fa fa-share"></i> Forward</button>
                   </div>
-              @endforeach
-
-              <h2>Add a new message</h2>
-              {!! Form::open(['method' => 'post', 'url' => 'updateThread/' . $thread->id]) !!}
-              <div class="col-md-6">
-                  <!-- Users Form Input -->
-                  <div class="form-group">
-                      {!! Form::label('users', 'Users', ['class' => 'control-label']) !!}
-                      {!! Form::text('users', null, ['id' =>  'users', 'name' => 'users', 'class' => 'form-control']) !!}
-                  </div>
-
-                  <!-- Subject Form Input -->
-                  <div class="form-group">
-                      {!! Form::hidden('subject', $thread->subject, ['class' => 'form-control']) !!}
-                  </div>
-
-                  <!-- Message Form Input -->
-                  <div class="form-group">
-                      {!! Form::label('message', 'Message', ['class' => 'control-label']) !!}
-                      {!! Form::textarea('message', null, ['class' => 'form-control']) !!}
-                  </div>
-
-                  <!-- Submit Form Input -->
-                  <div class="form-group">
-                      {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
-                  </div>
-              </div>
-              {!! Form::close() !!}
-
-          </div>
+                  <button class="btn btn-default"><i class="fa fa-trash-o"></i> Delete</button>
+                  <button class="btn btn-default"><i class="fa fa-print"></i> Print</button>
+                </div><!-- /.box-footer -->
+              </div><!-- /. box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
         </section><!-- /.content -->
       </div><!-- /.content-wrapper -->
 
